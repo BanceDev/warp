3c3f34b577e6ede666e61442d71ca5861b49fddb
BanceDev <lanceaborden@protonmail.com>
2025-02-27 17:14:43 -0500

    fixed install script

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ echo "Copying binary to /usr/local/bin ..."
 sudo cp ./target/release/edock /usr/local/bin
 
 # create the config directory
-sudo mkdir -p ~/.config/edock
+mkdir -p ~/.config/edock
 
 # copy style.css to the config directory
-sudo cp ./res/style.css ~/.config/edock
+cp ./res/style.css ~/.config/edock
 
 # copy the config file to the config directory
-sudo cp ./res/config ~/.config/edock
+cp ./res/config ~/.config/edock
