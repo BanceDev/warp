3c9b6f377a9ec0c6c2d9320daa08396b350cf643
BanceDev <lanceaborden@protonmail.com>
2025-02-28 14:53:01 -0500

    setting dock window properties

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,62 @@
+use gdk4_x11::X11Display;
+use gdk4_x11::X11Surface;
+use gdk4_x11::x11::xlib::{PropModeReplace, XA_ATOM, XChangeProperty, XInternAtom};
 use gtk::CssProvider;
 use gtk::gdk::Display;
 use gtk::glib;
 use gtk::prelude::*;
+use std::ffi::CString;
 use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
+use x11::xlib;
 use xdg::BaseDirectories;
 
 static APP_ID: &str = "dev.eidolon.edock";
-static mut OPEN_APPS: Vec<String> = Vec::new();
 
 #[derive(Debug, Clone)]
 struct App {
     icon: Option<String>,
     command: Option<String>,
+}
+
+fn set_utf8_props(window: &gtk::Window, prop_name: &str, prop_value: &str) {
+    let surface = window.surface().unwrap();
+    unsafe {
+        let xsurf = surface.unsafe_cast::<X11Surface>();
+        xsurf.set_utf8_property(prop_name, Some(prop_value));
+    }
+}
+
+fn set_window_props(window: &gtk::Window, prop_name: &str, prop_values: &Vec<&str>) {
+    let display = WidgetExt::display(window);
+    let surface = window.surface().unwrap();
+    let prop_name_cstr = CString::new(prop_name).unwrap();
+    let prop_values_cstr: Vec<CString> = prop_values
+        .iter()
+        .map(|val| CString::new(*val).unwrap())
+        .collect();
+    unsafe {
+        let xid: xlib::Window = surface.unsafe_cast::<X11Surface>().xid();
+        let xdisplay: *mut xlib::Display = display.unsafe_cast::<X11Display>().xdisplay();
+        let prop_name_atom = XInternAtom(xdisplay, prop_name_cstr.as_ptr(), xlib::False);
+        let mut prop_values_atom: Vec<u64> = prop_values_cstr
+            .into_iter()
+            .map(|cstr| XInternAtom(xdisplay, cstr.as_ptr(), xlib::False))
+            .collect();
+        let num_values = prop_values_atom.len();
+        let prop_values_c = prop_values_atom.as_mut_ptr();
+        XChangeProperty(
+            xdisplay,
+            xid,
+            prop_name_atom,
+            XA_ATOM,
+            32,
+            PropModeReplace,
+            prop_values_c as *const u8,
+            num_values as i32,
+        );
+    }
 }
 
 fn find_icon(icon_name: &str, icon_theme: Option<&str>, icon_size: Option<i64>) -> Option<String> {
@@ -106,6 +149,16 @@ fn build_ui(app: &gtk::Application) {
         .css_classes(["edock"])
         .build();
 
+    dock_window.connect_realize(move |dock_window| {
+        set_window_props(
+            dock_window.upcast_ref(),
+            "_NET_WM_WINDOW_TYPE",
+            &vec!["_NET_WM_WINDOW_TYPE_DOCK"],
+        );
+        set_utf8_props(dock_window.upcast_ref(), "_OB_APP_TYPE", "dock");
+        dock_window.present();
+    });
+
     let button_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
     button_box.set_css_classes(&["button"]);
 
@@ -127,7 +180,7 @@ fn build_ui(app: &gtk::Application) {
     }
 
     dock_window.set_child(Some(&button_box));
-    dock_window.present();
+    WidgetExt::realize(&dock_window);
 }
 
 fn main() -> glib::ExitCode {
